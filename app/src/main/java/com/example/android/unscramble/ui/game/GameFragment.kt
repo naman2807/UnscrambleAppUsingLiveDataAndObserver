75e68aa58928f1722d8018c7f4b3c1878fdaf82b
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-14 11:29:22 +0530

    At the end of the onViewCreated() method, attach an observer for the currentWordCount LiveData.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -56,10 +56,9 @@ class GameFragment : Fragment() {
         // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
-        // Update the UI
+        // Update the UI using observers
         viewModel.score.observe(viewLifecycleOwner, {score -> binding.score.text = getString(R.string.score, score)})
-        binding.wordCount.text = getString(
-                R.string.word_count, 0, MAX_NO_OF_WORDS)
+        viewModel.currentWordCount.observe(viewLifecycleOwner, {count -> getString(R.string.word_count, count, MAX_NO_OF_WORDS)})
         viewModel.currentScrambleWord.observe(viewLifecycleOwner, {name -> binding.textViewUnscrambledWord.text = name})
     }
 
